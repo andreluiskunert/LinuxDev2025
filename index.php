abfbfd2128217dc4b02b453cded54a85454c4d0e
AnaliseeDesenvolvimentodeSistemas <you@example.com>
2025-02-20 22:36:34 -0300

    LinuxDev

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/Projeto/CSS/estilos.css">
+    <title>LinuxDev</title>
+</head>
+  <header>
+     <div class="cabeca">
+        <h1>LinuxDev</h1>
+         <marquee> Bora Codar com LInux</marquee>
+     </div>
+
+  </header>
+
+<body> 
+    <div class="corpo">
+
+    </div>
+    
+</body>
+<footer>
+    <div class="ropade">
+
+    </div>
+</footer>
+</html>
